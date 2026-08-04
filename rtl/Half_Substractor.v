7e30b1dc9f63ac56981602128e2c4108290c75c3
manasvi28ap <manasvi2833@gmail.com>
2026-08-04 16:05:10 +0530

    Update Half_Substractor.v

--- rtl/Half_Substractor.v
+++ rtl/Half_Substractor.v
@@ -1,1 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04.08.2026 14:44:50
+// Design Name: 
+// Module Name: half_substractor
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module half_substractor(
+input A , B,
+output Diff,Bout
+    );
+    assign Diff=A ^ B;
+    assign Bout = ~A & B;
+endmodule
 
