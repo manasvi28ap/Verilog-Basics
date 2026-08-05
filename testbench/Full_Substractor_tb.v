05f40619f90f4e9e41780956264c50f4087d0c70
manasvi28ap <manasvi2833@gmail.com>
2026-08-05 21:57:26 +0530

    Update Full_Substractor_tb.v

--- testbench/Full_Substractor_tb.v
+++ testbench/Full_Substractor_tb.v
@@ -1,1 +1,48 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05.08.2026 21:51:48
+// Design Name: 
+// Module Name: full_substractor_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module full_substractor_tb();
+reg A , B, Bin;
+wire Diff, Bout;
+
+full_substractor uut(
+.A(A),
+.B(B),
+.Bin(Bin),
+.Diff(Diff),
+.Bout(Bout)
+);
+
+initial begin
+A = 0; B = 0; Bin = 0; #10;
+        A = 0; B = 0; Bin = 1; #10;
+        A = 0; B = 1; Bin = 0; #10;
+        A = 0; B = 1; Bin = 1; #10;
+        A = 1; B = 0; Bin = 0; #10;
+        A = 1; B = 0; Bin = 1; #10;
+        A = 1; B = 1; Bin = 0; #10;
+        A = 1; B = 1; Bin = 1; #10;
+        
+         $finish;
+end
+endmodule
 
